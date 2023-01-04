c4adfdd1eade05074a44280800302e332eaf1d70
Knight <82474150+RealKnight14@users.noreply.github.com>
2023-01-04 11:59:21 +0000

    Added the program and uploaded

---
+++ Move Picture Block/Move Picture Block/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Move_Picture_Block
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            runLoop();
+
+
+        }
+
+
+        public void runLoop()
+        {
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is Button)
+                {
+                    x.BackColor = Color.Black;
+                    x.ForeColor = Color.Black;
+                }
+
+                if (x is Label)
+                {
+                    x.ForeColor = Color.Purple;
+                }
+                if (x is PictureBox && (string)x.Tag == "group1")
+                {
+                    x.BackColor = Color.Plum;
+
+                }
+               if(x is PictureBox && (string)x.Tag == "group2")
+                {
+                    x.BackColor = Color.Maroon;
+                }
+            }
+
+        }
+
+
+
+
+    }
+
+}
